2d61b23cd1d6ba1500b94d5ac06f5beeb95f2ef9
dsy-sw <dsy94.sw@gmail.com>
2023-12-28 17:11:58 +0900

    Update middleware 1. create a function that updates the data.

--- src/obu/middleware.py
+++ src/obu/middleware.py
@@ -11,18 +11,31 @@ class Middleware:
 
         self.queue_data = deque([])
         
-        self.__init_obu_data()
+        self.__init_data()
 
-    def __init_obu_data(self):
+    def __init_data(self):
         self.l2id = 0
 
         self.bsm = BsmData()
-        self.cim = CimData()
+
+        self.vehicle_data = VehicleData()
     
     def _create_post_l2id(self, l2id: int):
 
+        self.cim = CimData(l2id)
         self.dnm_rep = DnmResponseData(l2id)
         self.dmm = DmmData(l2id)
         
     def interchange_data(self):
-        pass
+        self.vehicle_module
+
+    def update_data(self):
+        vehicle_data = self.vehicle_data
+        vehicle_module = self.vehicle_module
+        if not vehicle_module.is_connected:
+            return False
+        
+        vehicle_data.update_data(vehicle_module.get_data())
+        self.bsm.__dict__.update(vehicle_data.to_dict())
+
+        return True

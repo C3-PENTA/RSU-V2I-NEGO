5be159002c82ed3d9331192808d082c9126ed6eb
dsy-sw <dsy94.sw@gmail.com>
2024-01-08 20:04:44 +0900

    Fix middleware algorithm 1. update algorithm for receiving data l2id 2. seperate sending and receieving socket 3. fix data update algorithm

--- src/obu/middleware.py
+++ src/obu/middleware.py
@@ -1,47 +1,52 @@
 from collections import deque
 from time import sleep, time
 
-from ..bridge.socket import ObuSocket, VehicleSocket
+from ..bridge._socket import ObuSocket, VehicleSocket
 from config.parameter import ObuSocketParam, VehicleSocketParam, MiddleWareParam
 from .classes import *
 
 class Middleware:
     def __init__(self) -> None:
+        self.__init_data()
         self.config = MiddleWareParam
-        self.obu_module = ObuSocket(ObuSocketParam, self)
-        self.vehicle_module = VehicleSocket(VehicleSocketParam, self)
 
+        self.comm_state = False
         self.obu_queue = deque([])
         
-        self.__init_data()
-
+        
     def __init_data(self):
         self.l2id = 0
-
         self.bsm = BsmData()
-
+        self.cim = CimData(self.l2id)
         self.vehicle_data = VehicleData()
     
     def _create_post_l2id(self, l2id: int):
 
-        self.cim = CimData(l2id)
         self.dnm_rep = DnmResponseData(l2id)
         self.dmm = DmmData(l2id, self.vehicle_data.turn_signal)
+        self.cim.sender = self.l2id
         
     def unpack_msg_type(self, packet: bytes, _fmt: str = None) -> int:
         if _fmt is None:
-            _fmt = self.header_fmt
+            _fmt = self.bsm.header_fmt
         magic, msg_type, crc16, packet_len = unpack(_fmt, packet[:7])
         return msg_type
 
     def set_obu_data(self, data: bytes):
-        msg_type = self.unpack_msg_type(data)
+        msg_type = self.bsm.unpack_header(data)
+        # msg_type = self.unpack_msg_type(data)
+        obu_data = MSG_TYPE[msg_type](l2id = self.l2id)
+        obu_data.unpack_data(data)
         if msg_type == 4:
-            self.obu_module.send_obu_data(DnmResponseData(self.l2id))
+            self.receiver = obu_data.sender
+            self.obu_module.put_queue_data(DnmResponseData(self.l2id, self.receiver))
+        elif msg_type == 102:
+            self.l2id = obu_data.l2id
+            self.bsm.l2id = self.l2id
+            self.cim.sender = self.l2id
 
-        obu_data = MSG_TYPE[msg_type]().unpack_data(data)
+        self.vehicle_module.set_dict_data(obu_data.to_dict())
         
-
     def set_vehicle_data(self, data: dict):
         if not isinstance(data, dict):
             raise TypeError
@@ -65,25 +70,44 @@ class Middleware:
     def check_module_state(self)->bool:
         if not self.l2id:
             self.obu_module.put_queue_data(L2idRequestData())
+            self.comm_state = False
             return False
-        if not self.vehicle_module.is_connected:
-            print(f"Disconnected...")
-            return False
+        # if not self.vehicle_module.is_connected:
+        #     print(f"Disconnected...")
+        #     self.comm_state = False
+        #     return False
+        # self.comm_state = True
         return True
 
-
     def process(self):
+        self.vehicle_module = VehicleSocket(VehicleSocketParam)
+        self.obu_module = ObuSocket(ObuSocketParam, self)
         check_state = self.check_module_state
-        _interval = self.config.update_interval
-        
-        
+        _update_interval = self.config.update_interval
+        put_obu_queue = self.obu_module.put_queue_data
+
+
+        sync_time = time()
         while 1:
+            print(f"{self.bsm.l2id = }")
+            print(f"{self.l2id = }")
             if not check_state():
                 sleep(3)
                 continue
 
             _vehicle_data = self.vehicle_data
             if _vehicle_data.turn_signal:
-                self.obu_module.put_queue_data(DmmData(self.l2id, _vehicle_data.turn_signal))
+                put_obu_queue(DmmData(self.l2id, _vehicle_data.turn_signal))
 
+            dt = time() - sync_time
+            if _update_interval > dt:
+                sleep(_update_interval - dt)
+            sync_time = time()
             
+def run_middleware():
+    mw = Middleware()
+    mw.process()
+            
+if __name__ == '__main__':
+    mw = Middleware()
+    mw.process()

7a243600d8d27b48080a8c07407b2f7f9f58e9ac
dsy-sw <dsy94.sw@gmail.com>
2024-01-11 15:20:32 +0900

    Create test module that acts as an obu

--- src/tester/obu.py
+++ src/tester/obu.py
@@ -1,36 +1,81 @@
 import socket
 from time import time, sleep
+from collections import deque
+from random import choice
+from threading import Thread
 
 from config.obu_contant import DataFormat
 from config.parameter import RemoteAddress
 from src.obu.classes import *
+from src.tester.test_data import TEST_DATA, SEND_INTERVAL, SEND_RANDOM, SEND_DNM
 
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind(RemoteAddress.OBU_BIND)
-sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+class ObuTest():
+    def __init__(self) -> None:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.bind(RemoteAddress.OBU_BIND)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        sock.settimeout(0.2)
+        self.sock = sock
+        self.addr = None
+        self.l2id = 1234
+        self.is_l2id = False
 
-bsm = BsmData()
-_update_interval = 0.1
-sync_time = time()
-while 1:
-    recv_raw, addr = sock.recvfrom(1024)
-    msg_type = bsm.unpack_header(recv_raw)
-    # print(MSG_TYPE[msg_type](l2id = 1234))
-    if msg_type == 101:
-        a = L2idResponseData(1234)
-        sock.sendto(a.pack_data(DataFormat.BYTE_ORDER+DataFormat.HEADER+DataFormat.L2ID_RESPONSE), addr)
-        # print(f'{L2idResponseData(1234).pack_data(DataFormat.BYTE_ORDER+DataFormat.HEADER+DataFormat.L2ID_RESPONSE)}')
-    sock.sendto(bsm.pack_data(), addr)
-    
-    sock.sendto(EdmData().pack_data(), addr)
-    
-    sock.sendto(DnmRequestData().pack_data(), addr)
+        self._update_interval = 0.1
+        self.queue = deque([], maxlen=5)
 
-    sock.sendto(DnmDoneData().pack_data(), addr)
+    def recv_threading(self):
+        sock = self.sock
+        bsm = BsmData()
+        l2id = self.l2id
+        while 1:
+            try:
+                recv_raw, _addr = sock.recvfrom(1024)
+                self.addr = _addr
+                msg_type = bsm.unpack_header(recv_raw)                    
+                if msg_type == 101:
+                    self.queue.append(L2idResponseData(l2id).pack_data(DataFormat.BYTE_ORDER+DataFormat.HEADER+DataFormat.L2ID_RESPONSE))
+                    self.is_l2id = True
+                    print(f"{msg_type = }")
+                elif msg_type == 5:
+                    self.queue.append(TEST_DATA["DNM_DONE"])
+                elif msg_type == 8:
+                    print(f"{recv_raw = }")
+            except TimeoutError:
+                pass
 
+    def process(self):
+        recv_thread = Thread(target=self.recv_threading, daemon=True)
+        recv_thread.start()
+        # send_thread = Thread(target=self.send_threading, daemon=True, args=(sock, addr, is_l2id))
+        # send_thread.start()
+        sock = self.sock
+        _update_interval = self._update_interval
+        count = 0
+        sync_time = time()
+        while 1:
+            if self.addr is None or not self.is_l2id:
+                sleep(1)
+                continue
+            byte_data = choice(SEND_RANDOM)
+            sock.sendto(byte_data, self.addr)
+            if self.queue:
+                byte_data = self.queue.popleft()
+                sock.sendto(byte_data, self.addr)
+                
+            if not count%10:
+                byte_data = choice(SEND_INTERVAL)
+                sock.sendto(byte_data, self.addr)
+            if not count%20:
+                byte_data = SEND_DNM
+                sock.sendto(byte_data, self.addr)
+            count += 1
 
-    dt = time() - sync_time
-    if _update_interval > dt:
-        sleep(_update_interval - dt)
-    sync_time = time()
+            dt = time() - sync_time
+            if _update_interval > dt:
+                sleep(_update_interval - dt)
+            sync_time = time()
+
+if __name__ == "__main__":
+    test = ObuTest()
+    test.process()

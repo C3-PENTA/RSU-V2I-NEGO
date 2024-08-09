8099f4d4e8371d823cea87c1b41c53e4ec8531b9
dsy-sw <dsy94.sw@gmail.com>
2024-08-09 17:00:44 +0900

    Update middleware 1. manage obu data in a comprehensive manner 2. change one's name in detail 3. add algorithm to delete old obu data

--- src/obu/middleware.py
+++ src/obu/middleware.py
@@ -13,44 +13,63 @@ class Middleware:
 
         self.comm_state = False
         self.obu_queue = deque([])
-        
+        self.proximity_bsm = {}
+        self.proximity_rsu_data = {}
         
     def __init_data(self):
-        self.l2id = 0
-        self.bsm = BsmData()
-        self.cim = CimData(self.l2id)
+        self.ego_l2id = 0
+        self.ego_bsm = BsmData()
+        self.ego_bsm_light = BsmLightData()
+        self.cim = CimData(self.ego_l2id)
         self.vehicle_data = VehicleData()
     
     def _create_post_l2id(self, l2id: int):
 
         self.dnm_rep = DnmResponseData(l2id)
         self.dmm = DmmData(l2id, self.vehicle_data.turn_signal)
-        self.cim.sender = self.l2id
+        self.cim.sender = self.ego_l2id
         
     def unpack_msg_type(self, packet: bytes, _fmt: str = None) -> int:
         if _fmt is None:
-            _fmt = self.bsm.header_fmt
+            _fmt = self.ego_bsm.header_fmt
         magic, msg_type, crc16, packet_len = unpack(_fmt, packet[:7])
         return msg_type
+    
+    def delete_time_error_data(self, data):
+        if type(data) == dict:
+            current_timestamp = time()
+            for vehicle_l2id, vehicle_data in data.items():
+                vehicle_data: BsmData
+                if current_timestamp - vehicle_data.timestamp > 3:
+                    data.pop(vehicle_l2id)
 
     def set_obu_data(self, data: bytes):
-        msg_type = self.bsm.unpack_header(data)
+        msg_type = self.ego_bsm.unpack_header(data)
         # msg_type = self.unpack_msg_type(data)
-        obu_data = MSG_TYPE[msg_type](l2id = self.l2id)
+        obu_data = MSG_TYPE[msg_type](l2id = self.ego_l2id)
         obu_data.unpack_data(data)
         if msg_type == MessageType.DNM_REQUEST:
             self.receiver = obu_data.sender
-            self.obu_module.put_queue_data(DnmResponseData(self.l2id, self.receiver))
+            self.obu_module.put_queue_data(DnmResponseData(self.ego_l2id, self.receiver))
+            self.proximity_rsu_data[MessageType.DNM_REQUEST] = obu_data
         elif msg_type == MessageType.L2ID_RESPONSE:
-            self.l2id = obu_data.l2id
-            self.bsm.l2id = self.l2id
-            self.cim.sender = self.l2id
-
-        if obu_data.msg_type == MessageType.DMM_NOIT:
+            self.ego_l2id = obu_data.l2id
+            self.ego_bsm.l2id = self.ego_l2id
+            self.cim.sender = self.ego_l2id
+            self.proximity_rsu_data[MessageType.L2ID_RESPONSE] = obu_data
+        elif msg_type == MessageType.BSM_NOIT or MessageType.BSM_LIGHT_NOIT:
+            self.proximity_bsm[obu_data.l2id] = obu_data
+        elif msg_type == MessageType.DMM_NOIT:
             print(f"Receive DMM_NOIT from OBU: {obu_data}")
-        elif obu_data.msg_type == MessageType.EDM_NOIT:
+            self.vehicle_module.set_dict_data(obu_data)
+            self.proximity_rsu_data[MessageType.DMM_NOIT] = obu_data
+        elif msg_type == MessageType.EDM_NOIT:
             print(f"Receive EDM_NOIT from OBU: {obu_data}")
-        self.vehicle_module.set_dict_data(obu_data.to_dict())
+            self.vehicle_module.set_dict_data(obu_data)
+            self.proximity_rsu_data[MessageType.EDM_NOIT] = obu_data
+            
+        # 시나리오4에서 DNM Done을 수신해야 차선변경 가능
+        
         
     def set_vehicle_data(self, data: dict):
         if not isinstance(data, dict):
@@ -66,7 +85,9 @@ class Middleware:
             return False
         
         # vehicle_data.update_data(vehicle_module.get_data())
-        self.bsm.__dict__.update(vehicle_data.to_dict())
+        self.ego_bsm.__dict__.update(vehicle_data.to_dict())
+        if vehicle_data.turn_signal:
+            self.ego_bsm_light.__dict__.update(vehicle_data.to_dict())
 
         return True
     
@@ -74,7 +95,7 @@ class Middleware:
         self.obu_module.send_queue.append(argv)
 
     def check_module_state(self)->bool:
-        if not self.l2id:
+        if not self.ego_l2id:
             self.obu_module.put_queue_data(L2idRequestData())
             self.comm_state = False
             return False
@@ -86,7 +107,10 @@ class Middleware:
         check_state = self.check_module_state
         _update_interval = self.config.update_interval
         put_obu_queue = self.obu_module.put_queue_data
-
+        
+        _proximity_bsm = self.proximity_bsm
+        _proximity_rsu_data = self.proximity_rsu_data
+        _delete_time_error_data = self.delete_time_error_data
 
         sync_time = time()
         while 1:
@@ -96,8 +120,13 @@ class Middleware:
 
             _vehicle_data = self.vehicle_data
             if _vehicle_data.turn_signal:
-                put_obu_queue(DmmData(self.l2id, _vehicle_data.turn_signal))
-
+                put_obu_queue(DmmData(self.ego_l2id, _vehicle_data.turn_signal))
+            
+            if _proximity_bsm:
+                _delete_time_error_data(_proximity_bsm)
+            if _proximity_rsu_data:
+                _delete_time_error_data(_proximity_rsu_data)
+                
             dt = time() - sync_time
             if _update_interval > dt:
                 sleep(_update_interval - dt)

e5e07a892c502860f6ac0fdd5e6f05771c68125f
RSU_IONIQ <rsu@unmansol.com>
2024-11-13 11:47:57 +0900

    Delete unused files

--- main.py
+++ main.py
@@ -4,18 +4,13 @@ from threading import Thread
 from time import sleep
 
 from config.loggers import sys_log
-from origin.origin_module import run_rsu
 from src.obu.middleware import run_middleware
 
 module_list = {'rsu':Thread(),}
 def run_modules():
     while True:
         for name, module in module_list.items():
-            if module is None:
-                module_thread = Thread(target=run_rsu, daemon=True, name=name)
-                module_list[name] = module_thread
-                module_thread.start()
-            else:
+            if module is not None:
                 if not module.is_alive():
                     module_thread = Thread(target=run_middleware, daemon=True, name=name)
                     module_list[name] = module_thread
